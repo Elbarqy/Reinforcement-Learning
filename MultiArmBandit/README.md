693145a85576c425f15605fce4e696818168cca1
elbarqy <elbarky.dev@gmail.com>
2021-12-24 01:59:55 +0200

    implementing the UCB method to MABP

--- MultiArmBandit/README.md
+++ MultiArmBandit/README.md
@@ -1,13 +1,32 @@
-#Problem summary
+# Problem summary
 - There is a set of N random actions
 - Each action has its own distribution
 - The goal here is to estimate this distribution and get the best choice over time
 - When having minimum resources we are left with the problem of exploration vs exploitation
 - in which we conquer in this problem by using an epsilon value as a threshold for randomly exploring over exploiting the reward
 
-###TODO
-- [ ] implement the **UCB1** approach 
+### TODO
+- [x] implement the **UCB1** approach 
+  - Having an optimistic initialization of the reward Q encourages the agent to explore more and more
+  as it faces disappointment instead of encouragement with each reward. upper confidence bound action selection
+  takes advantage of this and also take into account long term reward by looking into the variance of the distribution
+  - Higher variance -> Higher potential for reward
+  - A = argmax(Q(a) + c*sqrt(ln(t)/Nt(a)))
+  - ln(t)  natural log of t steps
+  - number of times an actions is performed prior to t
+  - C control degree of exploration -> Hyper-parameter c>0
 - [ ] Apply real world application as an example to this problem
+- [ ] Change formulas to latex
 
 # Algorithm performance
-![alt text](result.png)
+![alt text](result_epsilonGreedy.png)
+
+## Side notes
+- **constant al**pha is better for non-stationary distributions**
+- Downside for optimistic initialization is that the algorithm doesn't take into account long term reward instead of short term
+- UCB1 notes : 
+  - when selecting an action -> uncertainty decreases
+  - on selecting something else -> uncertainty increases
+  - The algorithm should take into account if Nt(a) = 0 which means `a` the action estimate is a maximum
+  - All actions will be selected
+  - No epsilon greedy
--- MultiArmBandit/bandit.py
+++ MultiArmBandit/bandit.py
@@ -3,37 +3,56 @@ import matplotlib.pyplot as plt
 
 
 class Bandit(object):
-    def __init__(self, numArms, trueRewards, epsilon):
-        self.Q = [0 for _ in range(numArms)]
+    def __init__(self, numArms, trueRewards, epsilon, C, initialQ, mode):
+        self.Q = [initialQ for _ in range(numArms)]
         self.N = [0 for _ in range(numArms)]
         self.numArms = numArms
         self.epsilon = epsilon
         self.trueRewards = trueRewards
         self.lastAction = None
+        self.mode = mode
+        self.steps = 0
+        self.C = C
 
     def pull(self):
         rand = np.random.random()
         whichArm = None
-        if rand <= self.epsilon:
-            whichArm = np.random.choice(self.numArms)
-        elif rand > self.epsilon:
-            a = np.array([approx for approx in self.Q])
-            # choose a random arm with the same max value
-            whichArm = np.random.choice(np.where(a == a.max())[0])
+        if self.C == 0:  # following the epsilon greedy-way
+            if rand <= self.epsilon:
+                whichArm = np.random.choice(self.numArms)
+            elif rand > self.epsilon:
+                actionEstimate = np.array([approx for approx in self.Q])
+                # choose a random arm with the same max value
+                whichArm = np.random.choice(np.where(actionEstimate == actionEstimate.max())[0])
+        elif self.C > 0:  # UCB
+            actionEstimate = np.zeros(self.numArms)
+            for idx, approx in enumerate(self.Q):
+                if self.N[idx] != 0:
+                    actionEstimate[idx] = approx + self.C * np.sqrt(np.log(self.steps) / self.N[idx])
+                elif self.N[idx] == 0:  # taking into account the N[idx] == 0
+                    whichArm = idx
+                    break
+            else:
+                whichArm = np.random.choice(np.where(actionEstimate == actionEstimate.max())[0])
+
         self.lastAction = whichArm
+        self.steps += 1
         return np.random.randn() + self.trueRewards[whichArm]
 
     def updateMean(self, sample):
         whichArm = self.lastAction
         self.N[whichArm] += 1
-        self.Q[whichArm] = self.Q[whichArm] + 1.0 / self.N[whichArm] * (sample - self.Q[whichArm])
+        if self.mode == 'sample-average':
+            self.Q[whichArm] = self.Q[whichArm] + 1.0 / self.N[whichArm] * (sample - self.Q[whichArm])
+        elif self.mode == 'constant':
+            self.Q[whichArm] = self.Q[whichArm] + 0.1 * (sample - self.Q[whichArm])
 
 
-def simulate(numArms, epsilon, numPulls):
+def simulate(numArms, epsilon, numPulls, initialQ, C, mode):
     rewardHistory = np.zeros(numPulls)
     for j in range(2000):
         rewards = [np.random.randn() for _ in range(numArms)]
-        bandit = Bandit(numArms, rewards, epsilon)
+        bandit = Bandit(numArms, rewards, epsilon=epsilon, C=C, initialQ=initialQ, mode=mode)
         if j % 200 == 0:
             print(j)
         for i in range(numPulls):
@@ -47,11 +66,17 @@ def simulate(numArms, epsilon, numPulls):
 
 
 if __name__ == '__main__':
-    numActions = 5
-    run1 = simulate(numActions, epsilon=0.1, numPulls=1000)
-    run2 = simulate(numActions, epsilon=0.001, numPulls=1000)
-    run3 = simulate(numActions, epsilon=0.0, numPulls=1000)
-    plt.plot(run1, 'b--', run2, 'r--', run3, 'g--')
-    plt.legend(['epsilon=0.1', 'epsilon=0.01', 'epsilon=0.0'])
-    plt.savefig('result')
+    numActions = 10
+    ## old implementation
+    # run1 = simulate(numActions, epsilon=0.1, numPulls=1000)
+    # run2 = simulate(numActions, epsilon=0.001, numPulls=1000)
+    # run3 = simulate(numActions, epsilon=0.0, numPulls=1000)
+    # plt.plot(run1, 'b--', run2, 'r--', run3, 'g--')
+    # plt.savefig('result_epsilonGreedy')
+    # plt.legend(['epsilon=0.1', 'epsilon=0.01', 'epsilon=0.0'])
+    run1 = simulate(numActions, numPulls=1000, epsilon=0.1, C=0, initialQ=0, mode='constant')
+    run2 = simulate(numActions, numPulls=1000, epsilon=0.0, C=2, initialQ=0, mode='constant')
+    plt.plot(run1, 'b--', run2, 'r--')
+    plt.legend(['Epsilon Greedy', 'UCB C = 2'])
+    plt.savefig('result_UCB')
     plt.show()
